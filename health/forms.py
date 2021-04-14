9421687e4108aecb9c6402e9b3eb754a373c99c7
jasondiab <jjd13@mail.aub.edu>
2021-04-14 10:43:35 +0300

    Appointment system / new registration pages / updated homepage

---
+++ health/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import ConfirmedAppointment
+
+
+# creating a form
+class Apptform(forms.ModelForm):
+    # create meta class
+    class Meta:
+        # specify model to be used
+        model = ConfirmedAppointment
+
+        # specify fields to be used
+        fields = [
+            "time",
+            "name",
+            "drname",
+            "description"
+        ]
--- health/urls.py
+++ health/urls.py
@@ -3,14 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('i', views.index, name='index'),
-    path('', views.home, name='home'),
+    path('', views.index, name='home'),
+    path('oldUIhomepage', views.home, name='homeoldUI'),
     path('client', views.client, name='client'),
     path('doctor', views.doctor, name='doctor'),
     path('login', views.login, name='login'),
     path('appt',views.create_view, name='appt'),
     path('list', views.list_view, name='list'),
-    path('<id>', views.detail_view, name='detail'),
-    path('<id>/update', views.update_view, name='update'),
-    path('<id>/delete', views.delete_view, name='delete')
+    path('appt/<id>', views.detail_view, name='detail'),
+    path('appt/<id>/update', views.update_view, name='update'),
+    path('appt/<id>/delete', views.delete_view, name='delete')
 ]
