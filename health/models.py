89cbe72133c7dcef102461f3a66a437f6ddbd4b6
jasondiab <jjd13@mail.aub.edu>
2021-04-26 20:10:37 +0300

    login portal for drs button

--- health/models.py
+++ health/models.py
@@ -1,8 +1,8 @@
 from django import forms
 from django.db import models
 
-# Create your models here.
 
+# Create your models here.
 
 
 class ClientInfo(models.Model):
@@ -18,7 +18,6 @@ class ClientInfo(models.Model):
     weight = models.IntegerField()
 
 
-
 class DoctorInfo(models.Model):
     firstname = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
@@ -28,16 +27,10 @@ class DoctorInfo(models.Model):
     specialization = models.CharField(max_length=500)
     hospital = models.CharField(max_length=100)
 
+
 class ConfirmedAppointment(models.Model):
     time = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     drname = models.CharField(max_length=100)
     description = models.TextField()
-
-
-
-
-
-
-
